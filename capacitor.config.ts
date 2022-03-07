72e1a8a345760fa906e311d1f3dcafe26ab77457
Lorenzo <lodalmo@gmx.de>
2022-03-08 00:40:22 +0100

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.hardwarebuddy.com',
+  appName: 'Hardware Buddy',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
